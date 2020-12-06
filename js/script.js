40025e1f33292d4c89fd54077dfc73b0eaca89bd
Marcus Hermansen <marcushermansen90@gmail.com>
2020-12-06 23:21:44 +0100

    launch JOOD

---
+++ js/script.js
@@ -0,0 +1,11 @@
+AOS.init();
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:80,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true
+});
